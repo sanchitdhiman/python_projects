ddfd09a103f12244364e1e00367f1f220ee97068
sanchitdhiman <sanchitdhiman04@gmail.com>
2024-12-28 04:51:06 +0530

    Added modular functions to enhance modularity and code readability.

--- 2. Calculator.py
+++ 2. Calculator.py
@@ -1,33 +1,36 @@
-operation = input("Enter the operation you want to perform (+, -, *, /): ")
-
-if operation not in ['+', '-', '*', '/']:
-    print("Enter a valid operation nigga!")
-else:
-    try:
-        num1 = float(input("Enter the 1st operand: "))
-    except:
-        print("You don't know what numbers are? Are you fucking retarded?")
-    else:
+def get_input(prompt):
+    while True:
         try:
-            num2 = float(input("Enter the 2nd operand: "))
+            return float(input(prompt))
         except:
-            print("You don't know what numbers are? Are you fucking retarded?")
+            print("Invalid Input! Please enter a numeric value.")
+
+def calculator():
+    print("Welcome to the calculator program!")
+    print("\nAvaiable operations: +, -, *, /")
+
+    operation = input("\nEnter the operation you want to perform (+, -, *, /): ")
+
+    if operation not in ['+', '-', '*', '/']:
+        print("Invalid Input! Please enter a valid operation: +, -, *, /")
+        return
+
+    num1 = get_input("Enter the 1st operand: ")
+    num2 = get_input("Enter the 2nd operand: ")
+
+    if operation == '+':
+        result = num1 + num2
+        print(f"The sum of {num1} and {num2} is {result}.")
+    elif operation == '-':
+        result = num1 - num2
+        print(f"The difference between {num1} and {num2} is {result}.")
+    elif operation == '*':
+        result = num1 * num2
+        print(f"The product of {num1} and {num2} is {result}.")
+    elif operation == '/':
+        if num2 == 0:
+            print("Division by zero is not possible. Try again next time.")
         else:
-            if operation == '+':
-                result = num1 + num2
-                print(f"The sum of {num1} and {num2} is {result}.")
-            elif operation == '-':
-                result = num1 - num2
+            print(f"The quotient of {num1} and {num2} is {result}.")
 
-                print(f"The difference between {num1} and {num2} is {result}.")
-            elif operation == '*':
-                result = num1 * num2
-
-                print(f"The product of {num1} and {num2} is {result}.")
-            elif operation == '/':
-                try:
-                    result = num1 / num2
-                except:
-                    print("If you would have paid attention in class in the 6th grade, you would know that you can't divide by zero, you fucking moron!")
-                else:
-                    print(f"The quotient of {num1} and {num2} is {result}.")
+calculator()
