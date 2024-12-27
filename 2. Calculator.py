fe5623df944425f9d42bc32018bb51a6d1b8aac1
sanchitdhiman <sanchitdhiman04@gmail.com>
2024-12-28 04:32:33 +0530

    Added exceptional handling.

--- 2. Calculator.py
+++ 2. Calculator.py
@@ -1,32 +1,33 @@
 operation = input("Enter the operation you want to perform (+, -, *, /): ")
 
-if operation == '+':
-    num1 = float(input("Enter the 1st operand: "))
-    num2 = float(input("Enter the 2nd operand: "))
-
-    result = num1 + num2
-
-    print(f"The sum of {num1} and {num2} is {result}.")
-elif operation == '-':
-    num1 = float(input("Enter the 1st operand: "))
-    num2 = float(input("Enter the 2nd operand: "))
-
-    result = num1 - num2
-
-    print(f"The difference between {num1} and {num2} is {result}.")
-elif operation == '*':
-    num1 = float(input("Enter the 1st operand: "))
-    num2 = float(input("Enter the 2nd operand: "))
-
-    result = num1 * num2
-
-    print(f"The product of {num1} and {num2} is {result}.")
-elif operation == '/':
-    num1 = float(input("Enter the 1st operand: "))
-    num2 = float(input("Enter the 2nd operand: "))
-
-    result = num1 / num2
-
-    print(f"The quotient of {num1} and {num2} is {result}.")
-else:
+if operation not in ['+', '-', '*', '/']:
     print("Enter a valid operation nigga!")
+else:
+    try:
+        num1 = float(input("Enter the 1st operand: "))
+    except:
+        print("You don't know what numbers are? Are you fucking retarded?")
+    else:
+        try:
+            num2 = float(input("Enter the 2nd operand: "))
+        except:
+            print("You don't know what numbers are? Are you fucking retarded?")
+        else:
+            if operation == '+':
+                result = num1 + num2
+                print(f"The sum of {num1} and {num2} is {result}.")
+            elif operation == '-':
+                result = num1 - num2
+
+                print(f"The difference between {num1} and {num2} is {result}.")
+            elif operation == '*':
+                result = num1 * num2
+
+                print(f"The product of {num1} and {num2} is {result}.")
+            elif operation == '/':
+                try:
+                    result = num1 / num2
+                except:
+                    print("If you would have paid attention in class in the 6th grade, you would know that you can't divide by zero, you fucking moron!")
+                else:
+                    print(f"The quotient of {num1} and {num2} is {result}.")
